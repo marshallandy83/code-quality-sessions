e06980a4db5b3d965a1cc4d7d337e9f228dd5347
Andy Marshall <marshallandy83@gmail.com>
2022-04-28 13:47:40 +0100

    Only end local courses

--- SolidPrinciples/Tests/SRP/MedicationCourseEnderTests.cs
+++ SolidPrinciples/Tests/SRP/MedicationCourseEnderTests.cs
@@ -7,10 +7,12 @@ namespace Tests.SRP
 	public class MedicationCourseEnderTests
 	{
 		[Theory]
-		[InlineData(CourseStatus.Active, IssuanceStatus.Active, CourseStatus.Ended, "Prescribing error", IssuanceStatus.Cancelled, "Prescribing error")]
-		[InlineData(CourseStatus.Active, IssuanceStatus.Cancelled, CourseStatus.Ended, "Prescribing error", IssuanceStatus.Cancelled, null)]
-		[InlineData(CourseStatus.Ended, IssuanceStatus.Active, CourseStatus.Ended, null, IssuanceStatus.Active, null)]
+		[InlineData(Source.Local, CourseStatus.Active, IssuanceStatus.Active, CourseStatus.Ended, "Prescribing error", IssuanceStatus.Cancelled, "Prescribing error")]
+		[InlineData(Source.Local, CourseStatus.Active, IssuanceStatus.Cancelled, CourseStatus.Ended, "Prescribing error", IssuanceStatus.Cancelled, null)]
+		[InlineData(Source.Local, CourseStatus.Ended, IssuanceStatus.Active, CourseStatus.Ended, null, IssuanceStatus.Active, null)]
+		[InlineData(Source.External, CourseStatus.Active, IssuanceStatus.Active, CourseStatus.Active, null, IssuanceStatus.Active, null)]
 		public void EndTests(
+			Source courseAddedBy,
 			CourseStatus courseStatus,
 			IssuanceStatus issuanceStatus,
 			CourseStatus expectedCourseStatus,
@@ -19,7 +21,7 @@ namespace Tests.SRP
 			String expectedReasonForCancelling)
 		{
 			var issuance = new Issuance(issuanceStatus);
-			var course = new MedicationCourse("TestPreparation", courseStatus, new[] { issuance });
+			var course = new MedicationCourse("TestPreparation", courseStatus, new[] { issuance }, courseAddedBy);
 			var reasonForEnding = "Prescribing error";
 
 			new MedicationCourseEnder().End(course, reasonForEnding);
