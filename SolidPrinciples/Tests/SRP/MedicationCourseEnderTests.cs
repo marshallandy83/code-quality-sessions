f04d8bdd4385234abac2fa254599a0e4bf80eb12
Andy Marshall <marshallandy83@gmail.com>
2022-04-28 14:13:13 +0100

    Only cancel local issuances

--- SolidPrinciples/Tests/SRP/MedicationCourseEnderTests.cs
+++ SolidPrinciples/Tests/SRP/MedicationCourseEnderTests.cs
@@ -7,20 +7,22 @@ namespace Tests.SRP
 	public class MedicationCourseEnderTests
 	{
 		[Theory]
-		[InlineData(Source.Local, CourseStatus.Active, IssuanceStatus.Active, CourseStatus.Ended, "Prescribing error", IssuanceStatus.Cancelled, "Prescribing error")]
-		[InlineData(Source.Local, CourseStatus.Active, IssuanceStatus.Cancelled, CourseStatus.Ended, "Prescribing error", IssuanceStatus.Cancelled, null)]
-		[InlineData(Source.Local, CourseStatus.Ended, IssuanceStatus.Active, CourseStatus.Ended, null, IssuanceStatus.Active, null)]
-		[InlineData(Source.External, CourseStatus.Active, IssuanceStatus.Active, CourseStatus.Active, null, IssuanceStatus.Active, null)]
+		[InlineData(Source.Local, CourseStatus.Active, Source.Local, IssuanceStatus.Active, CourseStatus.Ended, "Prescribing error", IssuanceStatus.Cancelled, "Prescribing error")]
+		[InlineData(Source.Local, CourseStatus.Active, Source.Local, IssuanceStatus.Cancelled, CourseStatus.Ended, "Prescribing error", IssuanceStatus.Cancelled, null)]
+		[InlineData(Source.Local, CourseStatus.Ended, Source.Local, IssuanceStatus.Active, CourseStatus.Ended, null, IssuanceStatus.Active, null)]
+		[InlineData(Source.External, CourseStatus.Active, Source.Local, IssuanceStatus.Active, CourseStatus.Active, null, IssuanceStatus.Active, null)]
+		[InlineData(Source.Local, CourseStatus.Active, Source.External , IssuanceStatus.Active, CourseStatus.Ended, "Prescribing error", IssuanceStatus.Active, null)]
 		public void EndTests(
 			Source courseAddedBy,
 			CourseStatus courseStatus,
+			Source issuanceAddedBy,
 			IssuanceStatus issuanceStatus,
 			CourseStatus expectedCourseStatus,
 			String expectedReasonForEnding,
 			IssuanceStatus expectedIssuanceStatus,
 			String expectedReasonForCancelling)
 		{
-			var issuance = new Issuance(issuanceStatus);
+			var issuance = new Issuance(issuanceStatus, issuanceAddedBy);
 			var course = new MedicationCourse("TestPreparation", courseStatus, new[] { issuance }, courseAddedBy);
 			var reasonForEnding = "Prescribing error";
 
