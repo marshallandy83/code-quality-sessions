7d6385707e02484953b03dec045280600dad3a3d
Andy Marshall <marshallandy83@gmail.com>
2022-08-11 10:24:06 +0100

    Instantiate MedicationCourseEnder for different customers

--- SolidPrinciples/SRP/Program.cs
+++ SolidPrinciples/SRP/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace SRP
+﻿using System;
+
+namespace SRP
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		// An example of how the MedicationCourseEnder could be instantiated for different customers
+		static void Main(String[] args)
 		{
+			Int32 customerNumber = Int32.Parse(args[0]);
+
+			var courseEnder = new MedicationCourseEnder(
+				new ConsoleLogger(),
+				new MedicationIssuanceCanceller(
+					customerNumber == 1 ? (IMedicationIssuanceSelector)new ActiveAndLocalIssuancesSelector() : new ActiveIssuancesSelector()),
+				customerNumber == 1 ? (IMedicationCourseSelector)new ActiveAndLocalCoursesSelector() : new ActiveCoursesSelector());
 		}
 	}
 }
